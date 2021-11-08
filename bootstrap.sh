324143a1ca5a2f67133aa89fd3dc836bce4a272f
emanuel2718 <eramirez2718@gmail.com>
2021-11-08 07:09:51 -0400

    add spotify and 1password installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -188,18 +188,28 @@ install_pip_packages() {
 }
 
 install_external_packages() {
+    echo "- Installing Spotify"
+    sudo snap install spotify
+
     echo "- Installing CLion IDE"
     sudo snap install clion --classic
 
     echo "- Installing brave"
-
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
     sudo apt update
-
     sudo apt-get -qq install -y brave-browser 2> /dev/null
+
+
+    echo "- Installing 1Password"
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt update
+    sudo apt-get -qq install -y 1password 2> /dev/null
 }
 
 
