fce2dc17cfaa03a0bef703906808b95600de5df5
emanuel2718 <eramirez2718@gmail.com>
2021-11-28 13:23:41 -0400

    script: remove colored messages and fix dots bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,30 +7,12 @@
 
 script_location=$PWD
 
-# Color-coded script messages constants
-RED='\033[;31m'
-GREEN='\033[;32m'
-YELLOW='\033[;33m'
-NC='\033[0m'
-OK='\033[;32m[OK]\033[0m'
-INSTALLED='\033[;32m[INSTALLED]\033[0m'
-FAILED='\033[;31m[FAILED]\033[0m'
-NOTE='\033[;33m[NOTE]\033[0m'
-SUCCESS='\033[;32m[SUCCESS]\033[0m'
-LINKED='\033[;32m[LINKED]\033[0m'
-LINKED_FAILED='\033[;31m[LINK FAILED]\033[0m'
-UPDATING='\033[0;33m[UPDATING]\033[0m'
-UPDATED='\033[0;32m[UPDATED]\033[0m'
-INSTALLING='\033[0;33m[INSTALLING]\033[0m'
-DIR_CREATED='\033[0;32m[DIRECTORY+]\033[0m'
-EXECUTABLE='\033[0;33m[EXECUTABLE]\033[0m'
-
 
 update_system() {
-    echo -e "\033[;33m[UPDATING SYSTEM]\033[0m"
+    echo " > Updating system"
     sudo apt-get update --assume-yes >/dev/null
     sudo apt-get upgrade --assume-yes
-    echo -e "\033[;32m[SYSTEM UPDATED]\033[0m"
+    echo " > System updated"
 
     # Needed for snap package installation (sometimes?)
     if [ -f "/etc/apt/preferences.d/nosnap.pref" ]; then
@@ -89,7 +71,6 @@ apt_packages_to_install=(
 )
 
 install_apt_packages() {
-    echo -e "\033[;33m[INSTALLING BASE PACKAGES]\033[0m"
     for package in "${apt_packages_to_install[@]}"; do
 
         # install package
@@ -97,18 +78,12 @@ install_apt_packages() {
 
         # check if the package was installed succesfully
         if [ "$(dpkg -l | grep ${package} --no-message | wc -l)" -ge 1 ]; then
-            echo -e "${INSTALLED}  - ${package}"
+            echo " Installed - ${package}"
         else
-            echo -e "${FAILED}     - ${package}"
+            echo " Failed - ${package}"
         fi
     done
-
-    # add syslink to be able to use fd as a binary name
-    echo -e "${LINKED} - fd binary"
-    sudo ln -s $(which fdfind) $/HOME/.local/bin/fd
 }
-
-
 
 
 folders_to_create=(
@@ -139,9 +114,74 @@ folders_to_create=(
 
 create_folders() {
     for folder in "${folders_to_create[@]}"; do
-        echo -e "${DIR_CREATED} - ${folder}"
         mkdir -p ${folder}
+        echo " Directory Created - ${folder}"
     done
+}
+
+
+DOTFILES=(
+    ".bashrc"
+    ".fehbg"
+    ".inputrc"
+    ".profile"
+    ".tmux.conf"
+    ".vimrc"
+    ".xprofile"
+    ".xsession"
+    ".config/i3/bin/logout"
+    ".config/i3/bin/rofi_app_launcher"
+    ".config/i3/config"
+    ".config/i3status/config"
+    ".config/ranger/commands.py"
+    ".config/ranger/commands_full.py"
+    ".config/ranger/rc.conf"
+    ".config/ranger/rifle.conf"
+    ".config/ranger/scope.sh"
+    ".config/qutebrowser/config.py"
+    ".config/zathura/zathurarc"
+    ".config/nvim/init.lua"
+    ".config/nvim/ftplugin/lua.lua"
+    ".config/nvim/ftplugin/lua.vim"
+    ".config/nvim/lua/core_config/colorscheme.lua"
+    ".config/nvim/lua/core_config/keymaps.lua"
+    ".config/nvim/lua/core_config/lsp.lua"
+    ".config/nvim/lua/core_config/plugins.lua"
+    ".config/nvim/lua/core_config/settings.lua"
+    ".config/nvim/lua/core_plugins/airline/init.lua"
+    ".config/nvim/lua/core_plugins/airline/init.vim"
+    ".config/nvim/lua/core_plugins/autopairs/init.lua"
+    ".config/nvim/lua/core_plugins/compe/init.lua"
+    ".config/nvim/lua/core_plugins/lightline/init.lua"
+    ".config/nvim/lua/core_plugins/nerdcommenter/init.lua"
+    ".config/nvim/lua/core_plugins/nerdcommenter/init.vim"
+    ".config/nvim/lua/core_plugins/nvimcomment/init.lua"
+    ".config/nvim/lua/core_plugins/nvimtree/init.lua"
+    ".config/nvim/lua/core_plugins/nvimtree/init.vim"
+    ".config/nvim/lua/core_plugins/nvimtree/init.lua"
+    ".config/nvim/lua/core_plugins/telescope/init.lua"
+)
+
+install_dotfiles() {
+    for dotfile in "${DOTFILES[@]}"; do
+        rm -rf ${HOME}/${dotfile}
+        sudo ln -sf ${script_location}/${dotfile} ${HOME}/${dotfile}
+        if [ -L ${HOME}/${dotfile} ] && [ -e ${link} ]; then
+            echo " Linked -->  ${dotfile}"
+        else
+            echo " Link Failed -->  ${dotfile}"
+        fi
+    done
+
+    # add syslink to be able to use fd as a binary name
+    echo " Linked -->  fd binary"
+    sudo ln -sf $(which fdfind) $/HOME/.local/bin/fd
+
+    echo " > Copying wallpaper"
+    cp ${script_location}/wallpapers/wallpaper.jpg \
+    ${HOME}/Pictures/Wallpapers/pepoo_sad.jpg
+
+    source ${HOME}/.bashrc
 }
 
 excecutable_files=(
@@ -153,178 +193,120 @@ excecutable_files=(
     "${HOME}/.config/ranger/scope.sh"
 )
 
-DOTFILES=(
-    "/.bashrc"
-    "/.fehbg"
-    "/.inputrc"
-    "/.profile"
-    "/.tmux.conf"
-    "/.vimrc"
-    "/.xprofile"
-    "/.xsession"
-    "/.config/i3/bin/logout"
-    "/.config/i3/bin/rofi_app_launcher"
-    "/.config/i3/config"
-    "/.config/i3status/config"
-    "/.config/ranger/commands.py"
-    "/.config/ranger/commands_full.py"
-    "/.config/ranger/rc.conf"
-    "/.config/ranger/rifle.conf"
-    "/.config/ranger/scope.sh"
-    "/.config/qutebrowser/config.py"
-    "/.config/zathura/zathurarc"
-    "/.config/nvim/init.lua"
-    "/.config/nvim/ftplugin/lua.lua"
-    "/.config/nvim/ftplugin/lua.vim"
-    "/.config/nvim/lua/core_config/colorscheme.lua"
-    "/.config/nvim/lua/core_config/keymaps.lua"
-    "/.config/nvim/lua/core_config/lsp.lua"
-    "/.config/nvim/lua/core_config/plugins.lua"
-    "/.config/nvim/lua/core_config/settings.lua"
-    "/.config/nvim/lua/core_plugins/airline/init.lua"
-    "/.config/nvim/lua/core_plugins/airline/init.vim"
-    "/.config/nvim/lua/core_plugins/autopairs/init.lua"
-    "/.config/nvim/lua/core_plugins/compe/init.lua"
-    "/.config/nvim/lua/core_plugins/lightline/init.lua"
-    "/.config/nvim/lua/core_plugins/nerdcommenter/init.lua"
-    "/.config/nvim/lua/core_plugins/nerdcommenter/init.vim"
-    "/.config/nvim/lua/core_plugins/nvimcommnent/init.lua"
-    "/.config/nvim/lua/core_plugins/nvimtree/init.lua"
-    "/.config/nvim/lua/core_plugins/nvimtree/init.vim"
-    "/.config/nvim/lua/core_plugins/nvimtree/init.lua"
-    "/.config/nvim/lua/core_plugins/telescope/init.lua"
-)
-
-install_dotfiles() {
-    echo -e "${NOTE} - Copying dotfiles"
-    for dotfile in "${DOTFILES[@]}"; do
-        rm -rf ${HOME}/${dotfile}
-        ln -sf ${script_location}/${dotfile} ${HOME}/${dotfile}
-        if [ -L ${HOME}/${dotfile} ] && [ -e ${link} ]; then
-            echo -e "${LINKED} - ${dotfile}"
-        else
-            echo -e "${LINKED_FAILED} - ${dotfile}"
-        fi
-    done
-
-    echo -e "${SUCCESS} - Dotfiles installed succesfully"
-    source ${HOME}/.bashrc
-}
-
 make_files_executable() {
-    for exec in "${excecutable_files[@]}"; do
-        chmod +x ${exec}
-        echo -e "${EXECUTABLE} - ${exec}"
+    for ex in "${excecutable_files[@]}"; do
+        chmod +x ${ex}
+        echo " Executable - ${ex}"
     done
     source ${HOME}/.bashrc
 }
 
 install_plugins() {
-    echo -e "${INSTALLING} - vim pluggin manager"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo " Installed - vim pluggin manager"
 
     if [ ! -d ${HOME}/.config/autojump ]; then
-        echo -e "${INSTALLING} - autojump"
         git clone git://github.com/wting/autojump.git ${HOME}/.config/autojump
         cd $HOME/.config/autojump
         ./install.py
         cd $script_location
+        echo " Installed - autojump"
     else
-        echo -e "${NOTE} - Autojump is already installed"
+        echo " Note - Autojump is already installed"
     fi
 
     source $HOME/.bashrc
 }
 
 update_npm() {
-    echo -e "${UPDATING} - npm"
+    echo " Updating - npm"
     sudo npm i -g npm
     sudo npm install -g n
     sudo n stable
-    echo -e "${UPDATED} - npm"
+    echo " Updated  - npm"
 }
 
 install_pip() {
-    echo -e "${INSTALLING} - PIP"
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py
+    echo " Installed - pip"
 }
 
 install_pip_packages() {
 
-    echo -e "${INSTALLING} virtualenv"
+    echo " Installing - virtualenv"
     pip3 install -q virtualenv
-    echo -e "${INSTALLED} virtualenv"
+    echo " Installed  - virtualenv"
 
-    echo -e "${INSTALLING} ueberzu"
+    echo " Installing - ueberzu"
     pip3 install -q ueberzug
-    echo -e "${INSTALLED} ueberzu"
+    echo " Installed  - ueberzu"
 
-    echo -e "${INSTALLING} Jupyter Notebooks"
+    echo " Installing - Jupyter Notebooks"
     pip3 install -q jupyter
-    echo -e "${INSTALLED} Jupyter Notebooks"
+    echo " Installed  - Jupyter Notebooks"
 
-    echo -e "${INSTALLING} Jupyter-vim-binding plugin"
+    echo " Installing - Jupyter-vim-binding plugin"
     mkdir -p $(jupyter --data-dir)/nbextensions
     git clone https://github.com/lambdalisue/jupyter-vim-binding ${HOME}/vim_binding
     jupyter nbextension enable ${HOME}/vim_binding/vim_binding
-    echo -e "${INSTALLED} Jupyter-vim-binding plugin"
+    echo " Installed  - Jupyter-vim-binding plugin"
 }
 
 install_external_packages() {
-    echo -e "${INSTALLING} fzf from source"
+    echo " Installing - fzf"
     git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
     cd $HOME/.fzf/
     ./install --all
     cd ${script_location}
-    echo -e "${INSTALLED} fzf from source"
+    echo " Installed  - fzf"
 
 
-    echo -e "${INSTALLING} golang"
+    echo " Installing - Golang"
     sudo wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz $HOME/packages/&& sudo tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
-    echo -e "${INSTALLED} golang"
+    echo " Installed  - Golang"
 
-    echo -e "${INSTALLING} pyright"
+    echo " Installing - Pyright"
     sudo npm i -g pyright
-    echo -e "${INSTALLED} pyright"
+    echo " Installed  - Pyright"
 
-    echo -e "${INSTALLING} Compiling Neovim from source"
+    echo " Installing - Neovim"
     git clone https://github.com/neovim/neovim ${HOME}/packages/neovim
     cd ${HOME}/packages/neovim/ && sudo make -j4
     sudo make install
     cd ${script_location}
-    echo -e "${INSTALLED} Compiling Neovim from source"
+    echo " Installed  - Neovim"
 
-    echo -e "${INSTALLING} nvim.packer"
+    echo " Installing - nvim.packer"
     git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-    echo -e "${INSTALLED} nvim.packer"
+    echo " Installed  - nvim.packer"
 
-    echo -e "${INSTALLING} lazygit"
+    echo " Installing - lazygit"
     sudo add-apt-repository ppa:lazygit-team/release -y
     sudo apt-get update
     sudo apt-get install lazygit -y
-    echo -e "${INSTALLED} lazygit"
+    echo " Installed  - lazygit"
 
 
-    echo -e "${INSTALLING} Spotify"
+    echo " Installing - Spotify"
     sudo snap install spotify
-    echo -e "${INSTALLED} Spotify"
+    echo " Installed  - Spotify"
 
 
-    echo -e "${INSTALLING} Clion IDE"
+    echo " Installing - Clion IDE"
     sudo snap install clion --classic
-    echo -e "${INSTALLED} Clion IDE"
+    echo " Installed  - Clion IDE"
 
-    echo -e "${INSTALLING} Brave Browser"
+    echo " Installing - Brave Browser"
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update
     sudo apt-get -qq install -y brave-browser 2> /dev/null
-    echo -e "${INSTALLED} Brave Browser"
+    echo " Installed  - Brave Browser"
 
-    echo -e "${INSTALLING} 1Password"
+    echo " Installing - 1Password"
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
     echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
     sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
@@ -333,19 +315,19 @@ install_external_packages() {
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
     sudo apt update
     sudo apt-get -qq install -y 1password 2> /dev/null
-    echo -e "${INSTALLED} 1Password"
+    echo " Installed  - 1Password"
 
-    echo -e "${INSTALLING} Krita"
+    echo " Installing - Krita"
     sudo snap install krita
-    echo -e "${INSTALLED} Krita"
+    echo " Installed  - Krita"
 
-    echo -e "${INSTALLING} OBS"
+    echo " Installing - OBS"
     # fgmpeg is also required but it's already installed in the apt_install
     sudo apt install v4l2loopback-dkms -y
     sudo add-apt-repository ppa:obsproject/obs-studio
     sudo apt update
     sudo apt install obs-studio -y
-    echo -e "${INSTALLED} OBS"
+    echo " Installed  - OBS"
 }
 
 
@@ -399,16 +381,15 @@ native_emacs_req_packages=(
 
 
 install_native_emacs() {
-    echo -e "${INSTALLING} - Native Emacs required packages"
-    echo "- Installing Naitve Emacs required packages"
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa --asssume-yes
+    echo " > Installing Naitve Emacs required packages"
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa --assume-yes
     sudo apt update --assume-yes
     for package in "${native_emacs_req_packages[@]}"; do
         sudo apt-get -qq install -y "${package}" 2> /dev/null
-        echo -e "${INSTALLED} - ${package}"
+        echo  " Installed  - ${package}"
     done
 
-    echo -e "${NOTE} Cloning Emacs from github"
+    echo " > Cloning Emacs from github"
     git clone https://github.com/flatwhatson/emacs.git $HOME/emacs
 
     cd $HOME/emacs
@@ -427,30 +408,30 @@ install_native_emacs() {
 
     #./src/emacs
 
-    echo "> Back to dotmaker..."
+    echo " > Back to dotmaker..."
     cd $script_location
 
-    echo -e "${INSTALLING} - Doom Emacs"
+    echo " Installing - Doom Emacs"
     git clone --depth 1 https://github.com/hlissner/doom-emacs $HOME/.emacs.d
     $HOME/.emacs.d/bin/doom install
 }
 
 install_base_emacs() {
-    echo -e "${INSTALLING} - Base Emacs (Non-native compiled)"
+    echo " Installing - Base Emacs (Non-native compiled)"
     sudo apt-get -qq install -y emacs 2> /dev/null
-    echo -e "${INSTALLED}  - Base Emacs (Non-native compiled)"
+    echo " Installed  - Base Emacs (Non-native compiled)"
 
-    echo -e "${INSTALLING} - Doom Emacs"
+    echo " Installing - Doom Emacs"
     git clone --depth 1 https://github.com/hlissner/doom-emacs $HOME/.emacs.d
     $HOME/.emacs.d/bin/doom install
-    echo -e "${INSTALLED}  - Doom Emacs"
+    echo " Installed  - Doom Emacs"
 }
 
 install_emacs() {
     # Install Emacs (Native compilation or base Emacs)
     # The native compilation of Emacs might take 15 minutes or so. Beware of this when choosing.
     if [ ! -d ${HOME}/.emacs.d ]; then
-        echo -n "> Would you like to install native Emacs. This might take a while. (y/n)? "
+        echo -n " > Would you like to install native Emacs. This might take a while. (y/n)? "
         read answer
         if [ "$answer" != "${answer#[Yy]}" ]; then
             install_native_emacs
@@ -458,26 +439,26 @@ install_emacs() {
             install_base_emacs
         fi
 
-        echo "- Copying naysayer theme to ~/.emacs.d/themes/"
+        echo " > Copying naysayer theme to ~/.emacs.d/themes/"
         mkdir ${HOME}/.emacs.d/themes
         cp ${script_location}/emacs_themes/naysayer-theme.el ${HOME}/.emacs.d/themes
     else
-        echo "> Emacs already installed in this system"
+        echo " > Emacs already installed in this system"
     fi
 
     # Install personal doom configuration files
     if [ -d ${HOME}/.doom.d ]; then
         rm -rf ${HOME}/.doom.d/config.el
-        ln -sf ${script_location}/.doom.d/config.el ${HOME}/.doom.d/config.el
-        echo "${LINKED} - ${HOME}/.doom.d/config.el"
+        sudo ln -sf ${script_location}/.doom.d/config.el ${HOME}/.doom.d/config.el
+        echo " Linked -->  ${HOME}/.doom.d/config.el"
 
         rm -rf ${HOME}/.doom.d/init.el
-        ln -sf ${script_location}/.doom.d/init.el ${HOME}/.doom.d/init.el
-        echo "${LINKED} - ${HOME}/.doom.d/init.el"
+        sudo ln -sf ${script_location}/.doom.d/init.el ${HOME}/.doom.d/init.el
+        echo " Linked -->  ${HOME}/.doom.d/init.el"
 
         rm -rf ${HOME}/.doom.d/packages.el
-        ln -sf ${script_location}/.doom.d/packages.el ${HOME}/.doom.d/packages.el
-        echo "${LINKED} - ${HOME}/.doom.d/packages.el"
+        sudo ln -sf ${script_location}/.doom.d/packages.el ${HOME}/.doom.d/packages.el
+        echo " Linked -->  ${HOME}/.doom.d/packages.el"
     fi
 }
 
