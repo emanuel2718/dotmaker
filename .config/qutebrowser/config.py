d475b9373eb158337c07e326f4c6021ff1ef218a
emanuel2718 <eramirez2718@gmail.com>
2022-01-28 09:03:38 -0400

    update i3 config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,7 +4,7 @@ from qutebrowser.api import interceptor
 
 
 # ================== DEFAULTS =========================
-config.load_autoconfig(False)
+#config.load_autoconfig(False)
 
 c.auto_save.session = False
 c.statusbar.widgets = ['keypress', 'history', 'progress']
@@ -94,18 +94,18 @@ c.aliases['gg'] = 'open https://github.com/emanuel2718'
 
 
 # ================== Youtube Add Blocking =======================
-def filter_yt(info: interceptor.Request):
-    """Block the given request if necessary."""
-    url = info.request_url
-    if (
-        url.host() == "www.youtube.com"
-        and url.path() == "/get_video_info"
-        and "&adformat=" in url.query()
-    ):
-        info.block()
-
-
-interceptor.register(filter_yt)
+#def filter_yt(info: interceptor.Request):
+#    """Block the given request if necessary."""
+#    url = info.request_url
+#    if (
+#        url.host() == "www.youtube.com"
+#        and url.path() == "/get_video_info"
+#        and "&adformat=" in url.query()
+#    ):
+#        info.block()
+#
+#
+#interceptor.register(filter_yt)
 # ===============================================================
 
 # ====================== COLORS =======================
