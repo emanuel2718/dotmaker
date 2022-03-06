acbf5a59b8c838c8f543ba8f31b6914d04097c3a
emanuel2718 <eramirez2718@gmail.com>
2022-03-06 12:43:12 -0400

    remove unwanted lsp config lines

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -135,20 +135,12 @@
               ("C-c C-c Q" . lsp-workspace-shutdown)
               ("C-c C-c s" . lsp-rust-analyzer-status))
   :config
-  ;; uncomment for less flashiness
-  (setq lsp-eldoc-hook nil)
-  (setq lsp-enable-symbol-highlighting nil)
-  (setq lsp-signature-auto-activate nil)
 
   ;; comment to disable rustfmt on save
   (setq rustic-format-on-save t)
   (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
 
 (defun rk/rustic-mode-hook ()
-  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
-  ;; save rust buffers that are not file visiting. Once
-  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
-  ;; no longer be necessary.
   (when buffer-file-name
     (setq-local buffer-save-without-query t)))
 
