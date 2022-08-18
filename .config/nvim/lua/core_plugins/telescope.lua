307659172be1e6671c8fcd3b158b97de270ea07f
emanuel2718 <eramirez2718@gmail.com>
2022-08-18 09:58:35 -0400

    ignore node_modules in telescope

--- .config/nvim/lua/core_plugins/telescope.lua
+++ .config/nvim/lua/core_plugins/telescope.lua
@@ -5,11 +5,10 @@ require('telescope').setup({
 		file_sorter = require('telescope.sorters').get_fzy_sorter,
 		prompt_prefix = '  ',
 		color_devicons = true,
-    file_ignore_patterns = { 'yarn.lock' },
+    file_ignore_patterns = { 'node_modules' },
 		file_previewer = require('telescope.previewers').vim_buffer_cat.new,
 		grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
 		qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-
 		mappings = {
 			i = {
 				['<C-x>'] = false,
