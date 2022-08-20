c41f207826a39e761d1eb9bc252f5ef3ba5ec8e2
emanuel2718 <eramirez2718@gmail.com>
2022-08-20 13:03:27 -0400

    update treesitter plugin config

--- .config/nvim/lua/core_plugins/treesitter.lua
+++ .config/nvim/lua/core_plugins/treesitter.lua
@@ -1,43 +1,58 @@
 require('nvim-treesitter.configs').setup{
-
-  ensure_installed = {'c', 'cpp', 'python', 'lua', 'typescript',
-                      'regex', 'bash', 'cmake', 'css', 'javascript',
-                      'html', 'comment', 'java', 'rust', 'go', 'markdown',
-                      'make', 'json', 'vim'},
+  ensure_installed = 'all',
+  sync_install = false,
+  ingnore_install = false,
   highlight = {
     enable = true,
-    disable = {},
+    disable = {''},
+    additional_vim_regex_highlighting = true,
   },
-
-  rainbow = {
-    enable = false,
-    -- disable = { 'jsx', 'cpp' },
-    extended_mode = true,
-    max_file_lines = nil,
-    -- colors = {},
-    -- termcolors = {}
-  },
-
-  autotag = {
+  indent = { enable = true, disable = {'yaml'}},
+  context_commentstring = {
     enable = true,
-  },
-
-  refactor = {
-    smart_rename = {
-      enable = true,
-      keymaps = {
-        smart_rename = 'grr',
-      },
-    },
-    navigation = {
-      enable = true,
-      keymaps = {
-        goto_definition = 'gnd',
-        list_definitions = 'gnD',
-        list_definitions_toc = 'gO',
-        goto_next_usage = '<A-]>',
-        goto_previous_usage = '<A-[>',
-      },
-    },
-  },
+    enable_autocmd = true
+  }
 }
+-- require('nvim-treesitter.configs').setup{
+
+--   ensure_installed = {'c', 'cpp', 'python', 'lua', 'typescript',
+--                       'regex', 'bash', 'cmake', 'css', 'javascript',
+--                       'html', 'comment', 'java', 'rust', 'go', 'markdown',
+--                       'make', 'json', 'vim'},
+--   highlight = {
+--     enable = true,
+--     disable = {},
+--   },
+
+--   rainbow = {
+--     enable = false,
+--     -- disable = { 'jsx', 'cpp' },
+--     extended_mode = true,
+--     max_file_lines = nil,
+--     -- colors = {},
+--     -- termcolors = {}
+--   },
+
+--   autotag = {
+--     enable = true,
+--   },
+
+--   refactor = {
+--     smart_rename = {
+--       enable = true,
+--       keymaps = {
+--         smart_rename = 'grr',
+--       },
+--     },
+--     navigation = {
+--       enable = true,
+--       keymaps = {
+--         goto_definition = 'gnd',
+--         list_definitions = 'gnD',
+--         list_definitions_toc = 'gO',
+--         goto_next_usage = '<A-]>',
+--         goto_previous_usage = '<A-[>',
+--       },
+--     },
+--   },
+-- }
