066f2b07c818c244f8315bb37f0a5fbe51e6c142
emanuel2718 <eramirez2718@gmail.com>
2022-08-28 11:48:09 -0400

    change copilot accept keybind

--- .config/nvim/lua/core_plugins/copilot.lua
+++ .config/nvim/lua/core_plugins/copilot.lua
@@ -4,4 +4,4 @@ vim.g.copilot_assume_mapped = true
 -- vim.g.copilot_tab_fallback = ""
 vim.g.copilot_node_command = "~/.nodenv/versions/16.15.0/bin/node"
 
-vim.api.nvim_set_keymap('i', '<Tab>', 'copilot#Accept()', { expr = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-p>', 'copilot#Accept()', { expr = true, silent = true})
