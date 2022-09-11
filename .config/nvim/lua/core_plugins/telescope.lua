15a2868c7faebf8f0cabb6baed35ae8df02b4d7d
emanuel2718 <emanuel@coquihealth.com>
2022-09-11 18:29:58 -0400

    improve telescope handling

--- .config/nvim/lua/core_plugins/telescope.lua
+++ .config/nvim/lua/core_plugins/telescope.lua
@@ -5,14 +5,18 @@ require('telescope').setup({
 		file_sorter = require('telescope.sorters').get_fzy_sorter,
 		prompt_prefix = '  ',
 		color_devicons = true,
-    file_ignore_patterns = { 'node_modules', 'package-lock.json' },
+    file_ignore_patterns = { 'node_modules' },
 		file_previewer = require('telescope.previewers').vim_buffer_cat.new,
 		grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
 		qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+
 		mappings = {
 			i = {
 				['<C-x>'] = false,
 				['<C-q>'] = actions.send_to_qflist,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<esc>'] = actions.close,
 			},
       n = {
         ['<c-d>'] = require('telescope.actions').delete_buffer
