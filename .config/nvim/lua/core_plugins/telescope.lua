d24410f11ac136368b5524ff90591d2a2b327daa
emanuel2718 <eramirez2718@gmail.com>
2022-11-19 10:25:26 -0400

    update telescope config

--- .config/nvim/lua/core_plugins/telescope.lua
+++ .config/nvim/lua/core_plugins/telescope.lua
@@ -3,63 +3,127 @@ local actions = require('telescope.actions')
 require('telescope').setup({
   pickers = {
     find_files = {
-      theme = 'ivy'
-    },
-    git_files = {
-      theme = 'ivy'
+      theme = "dropdown",
+      hidden = true,
+      previewer = false,
     },
     live_grep = {
-      theme = 'ivy'
+      --@usage don't include the filename in the search results
+      only_sort_text = true,
+      theme = "dropdown",
     },
-    oldfiles = {
-      theme = 'ivy'
-    },
-    current_buffer_fuzzy_find = {
-      theme = 'ivy'
+    grep_string = {
+      only_sort_text = true,
+      theme = "dropdown",
     },
     buffers = {
-      theme = 'ivy'
+      theme = "dropdown",
+      previewer = false,
+      initial_mode = "normal",
+      mappings = {
+        i = {
+          ["<C-d>"] = actions.delete_buffer,
+        },
+        n = {
+          ["dd"] = actions.delete_buffer,
+        },
+      },
     },
-    git_status = {
-      theme = 'ivy'
+    planets = {
+      show_pluto = true,
+      show_moon = true,
     },
-    git_branches = {
-      theme = 'ivy'
+    git_files = {
+      theme = "dropdown",
+      hidden = true,
+      previewer = false,
+      show_untracked = true,
+    },
+    lsp_references = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_definitions = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_declarations = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_implementations = {
+      theme = "dropdown",
+      initial_mode = "normal",
     },
   },
-	defaults = {
-		file_sorter = require('telescope.sorters').get_fzy_sorter,
+  defaults = {
+    file_sorter = require('telescope.sorters').get_fzy_sorter,
     prompt_prefix = " ",
     selection_caret = "> ",
     path_display = { "smart" },
-		color_devicons = true,
-    file_ignore_patterns = { 'node_modules', 'git' },
-		file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-		grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-		qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    color_devicons = true,
+    -- file_ignore_patterns = { 'node_modules', 'git' },
+    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      width = 0.75,
+      preview_cutoff = 120,
+      horizontal = {
+        preview_width = function(_, cols, _)
+          if cols < 120 then
+            return math.floor(cols * 0.5)
+          end
+          return math.floor(cols * 0.6)
+        end,
+        mirror = false,
+      },
+      vertical = { mirror = false },
+    },
+    file_ignore_patterns = { 'node_modules' },
+    path_display = { "smart" },
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--glob=!.git/",
+    },
 
-		mappings = {
-			i = {
-				['<C-x>'] = false,
-				['<C-q>'] = actions.send_to_qflist,
+    mappings = {
+      i = {
+        ['<C-x>'] = false,
+        ['<C-q>'] = actions.send_to_qflist,
         ['<C-j>'] = actions.move_selection_next,
         ['<C-k>'] = actions.move_selection_previous,
         ['<esc>'] = actions.close,
         ['<C-/>'] = actions.which_key,
         ["<c-d>"] = require("telescope.actions").delete_buffer,
-			},
+      },
       n = {
         ['<c-d>'] = require('telescope.actions').delete_buffer
 
       }
-		},
-	},
-	-- extensions = {
-	-- 	fzy = {
-	--             fuzzy = true,
-	-- 		override_generic_sorter = false,
-	-- 		override_file_sorter = true,
-	--             case_mode = 'smart_case',
-	-- 	},
-	-- },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+    },
+  },
 })
