201be98183f2c222e6952f4f785f47939e17fc41
emanuel2718 <eramirez2718@gmail.com>
2023-10-07 00:30:56 -0400

    add partial nvim config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,4 @@
+return {
+ -- Carbonfox
+ { "EdenEast/nightfox.nvim" }
+}
---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,37 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+  opts = {
+    PATH = "prepend",
+    max_concurrent_installers = 10,
+  },
+  config = function()
+    local mason_tool_installer = require("mason-tool-installer")
+    local mason = require("mason")
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint", -- python linter
+        "eslint_d", -- js linter
+      },
+      run_on_start = true,
+      debounce_hours = 24
+    })
+  end,
+}
