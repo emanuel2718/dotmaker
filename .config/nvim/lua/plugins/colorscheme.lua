892e685cbe5baca6ad335eb43f3b363d44e59aa8
Emanuel Ramírez <55965894+emanuel2718@users.noreply.github.com>
2023-11-19 10:56:26 -0500

    nvim: update gruvbox-material settings

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,51 @@
 return {
-  { "sainnhe/gruvbox-material" },
+  {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_better_performance = 1
+      -- Fonts
+      vim.g.gruvbox_material_disable_italic_comment = 1
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.g.gruvbox_material_enable_bold = 0
+      vim.g.gruvbox_material_transparent_background = 1
+      -- Themes
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
+
+    end,
+  },
   { "EdenEast/nightfox.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
-  { 'projekt0n/github-nvim-theme' },
-  { 'ishan9299/nvim-solarized-lua' },
-  { "rockyzhang24/arctic.nvim", dependencies = { "rktjmp/lush.nvim" }, name = "arctic", branch = "main", priority = 1000 }
+  { "projekt0n/github-nvim-theme" },
+  { "ishan9299/nvim-solarized-lua" },
+  {
+    "rockyzhang24/arctic.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    name = "arctic",
+    branch = "main",
+    priority = 1000,
+  },
+  { "dasupradyumna/midnight.nvim", lazy = false, priority = 1000 },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
