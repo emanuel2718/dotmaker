9dec213dd05fe766fd3aff99903bc55acdaa525c
emanuel2718 <eramirez2718@gmail.com>
2023-12-31 15:49:24 -0400

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,8 @@ abbr -a ... 'cd ../..'
 abbr -a .... 'cd ../../..'
 abbr -a cal 'cal -y'
 abbr -a i3config 'nvim $HOME/.config/i3/config'
-abbr -a nrc 'nvim $HOME/.config/nvim'
 abbr -a rc 'nvim $HOME/.config/fish/config.fish'
 abbr -a src 'source $HOME/.config/fish/config.fish'
-abbr -a gsync 'git stash; and git pull --rebase; and git stash pop'
 abbr -a tk 'tmux kill-session -t'
 abbr -a ta 'tmux a -t'
 abbr -a tn 'tmux new -s'
@@ -19,11 +17,50 @@ abbr -a vpnd 'mullvad disconnect'
 abbr -a vpnl 'mullvad relay set location'
 abbr -a ct 'cargo test'
 abbr -a cr 'cargo run'
+abbr -a pbcopy 'xsel --clipboard --input'
+abbr -a pbpaste 'xsel --clipboard --output'
 
+abbr -a gsync 'git stash; and git pull --rebase; and git stash pop'
+abbr -a ga 'git add'
+abbr -a gs 'git status'
+abbr -a gc 'git commit'
+abbr -a gco 'git checkout'
+abbr -a gl 'git prettylog'
+abbr -a gp 'git push'
+abbr -a gt 'git tag'
+
+abbr -a ng 'nvim +Neogit'
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+
+set -U fish_color_normal normal
+set -U fish_color_command F8F8F2
+set -U fish_color_quote F1FA8C
+set -U fish_color_redirection 8BE9FD
+set -U fish_color_end 50FA7B
+set -U fish_color_error FF5555
+set -U fish_color_param 5FFFFF
+set -U fish_color_comment 6272A4
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion BD93F9
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
 
 
 set -g fish_greeting
@@ -34,28 +71,8 @@ fish_add_path $PYENV_ROOT/bin
 
 source $HOME/.xprofile
 
-set -Ux PROJECTS $HOME/dev/code
-
-
-function fish_prompt
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (hostnamectl hostname)
-	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
-		set_color yellow
-		echo -n (basename $PWD)
-	end
-	set_color green
-	printf '%s ' (__fish_git_prompt)
-	set_color red
-	echo -n '| '
-	set_color normal
-end
-
-set --universal EDITOR /usr/local/bin/vim
+set -Ux PROJECTS $HOME/dev
+set --universal EDITOR nvim
 
 setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
 setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
@@ -66,3 +83,6 @@ set -Ua fish_user_paths $HOME/.cargo/bin
 zoxide init fish | source
 starship init fish | source
 pyenv init - | source
+
+# Created by `pipx` on 2023-12-17 16:49:28
+set PATH $PATH /home/rami/.local/bin
