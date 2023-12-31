1330d825e7ecc5af61a369104f8f8640ee29a25a
emanuel2718 <emanuel@coquihealth.com>
2023-12-31 14:58:27 -0400

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,17 +1,28 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-- config.font = wezterm.font("FiraCode Nerd Font")
+-- config.font = wezterm.font("D2Coding Nerd Font")
+config.font = wezterm.font("Hasklug Nerd Font")
+config.font_size = 15.0
+config.warn_about_missing_glyphs = false
+config.color_scheme = 'Nord (base16)'
+-- config.color_scheme = 'nord'
+
+config.animation_fps = 1
+
+config.enable_tab_bar = false
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+
 config.window_close_confirmation = "NeverPrompt"
-
-
-config.font = wezterm.font 'FiraCode Nerd Font'
-config.warn_about_missing_glyphs=false
+config.audible_bell = "Disabled"
+config.cursor_thickness = "1"
 
 config.enable_scroll_bar = false
 config.adjust_window_size_when_changing_font_size = false
@@ -20,6 +31,10 @@ config.window_padding = {
   right = 0,
   top = 0,
   bottom = 0,
+}
+
+config.keys = {
+  { key = "/", mods = "SUPER", action = act.Search("CurrentSelectionOrEmptyString") },
 }
 
 return config
