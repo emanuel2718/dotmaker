c62849752312baa67695a3d37d13d93d548d7ac8
emanuel2718 <eramirez2718@gmail.com>
2024-01-07 08:07:55 -0400

    nvim: remove nvim.old

--- .config/nvim.old/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-local modules = { "lazy", "autocmds", "keymaps", "options", "theme" }
-for _, mod in ipairs(modules) do
-  require("config." .. mod)
-end
--- .config/nvim.old/lua/plugins/toggle-diagnostics.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
-  config = function()
-    require("toggle_lsp_diagnostics").init({ start_on = false })
-    local map = vim.keymap.set
-    local opts = { noremap = false, silent = true }
-    map("n", "<leader>dd", "<cmd>ToggleDiag<cr>", opts)
-  end,
-}
