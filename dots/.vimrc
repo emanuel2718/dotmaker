eab2edbd4cfc2a4612fceef6065671cbae7d76a1
emanuel2718 <eramirez2718@gmail.com>
2025-03-29 20:10:30 -0400

    sync changes

---
+++ dots/.vimrc
@@ -0,0 +1,17 @@
+filetype plugin indent on
+syntax enable
+
+set number
+set ruler
+set expandtab
+set autoindent
+set smartindent
+set tabstop=2
+set softtabstop=2
+set shiftwidth=2
+
+set nowritebackup
+set noswapfile
+set nobackup
+
+set rtp+=/usr/local/opt/fzf
--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 # Make the screenshot folder
-
 mkdir -p ~/Documents/Screenshots
 defaults write com.apple.screencapture location /path/to/screenshots/ && killall SystemUIServer
+
+# Make needed folders
+mkdir -p ~/git
+mkdir -p ~/git/oss
 
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -36,19 +39,47 @@ Host github.com
 git config --global core.editor "vim"
 git config --global init.defaultBranch master
 git config --global --add --bool push.autoSetupRemote true
+git config --global core.excludesfile ~/.gitignore
 git config --global user.name "emanuel2718"
 git config --global user.email "eramirez2718@gmail.com"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
 
 
 # BREW
 brew install htop
 brew install btop
 brew install uv
+brew install cmake
+brew install coreutils
+brew install tmux
+brew install ripgrep
+brew install direnv
+brew install ffmpeg
+brew install wget
+brew install jq
+brew install neofetch
 brew install node
 brew install nvm
 brew install stats
 brew install jordanbaird-ice
 brew install zoxide
+brew install tree
+brew install fzf
+brew install libvirt
+brew install virt-manager
+brew install qemu
+brew install ninja
+brew install cmake
+brew install gettext
+brew install curl
+
+
+
+# Probably will be a good idea to install them like this
+# brew install --cask \
+#   raycast \
+#   google-chrome  \
 
 # casks
 brew install --cask 1password
@@ -68,7 +99,16 @@ brew install --cask appcleaner
 brew install --cask transmission
 brew install --cask chatgpt
 brew install --cask bettertouchtool
+brew install --cask sublime-text
+brew install --cask visual-studio-code
+brew install --cask focusrite-control
+brew install --cask screenflow
+brew install --cask whatsapp
+brew install --cask bruno
 
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # All of this must come after the brew installs
 brew install uv
@@ -76,3 +116,20 @@ uv python install 3.12
 
 # install nvm
 brew install node
+
+
+# install nvim from source
+cd ~/git/oss
+git clone https://github.com/neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+
+
+
+# Symlink files
+ln -s -f ~/git/dotmaker/.gitignore $HOME/.gitignore
+ln -s -f ~/git/dotmaker/dots/.vimrc $HOME/.vimrc
+ln -s -f ~/git/dotmaker/dots/.zshrc $HOME/.zshrc
+
+
