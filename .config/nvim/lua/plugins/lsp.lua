9fa3f288856c732a6421b46b05504c982a0cae19
emanuel2718 <eramirez2718@gmail.com>
2025-03-30 21:24:53 -0400

    remove unwanted comments in lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ return {
     { "williamboman/mason.nvim", config = true },
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    { "j-hui/fidget.nvim", opts = {} },
+    { "j-hui/fidget.nvim",       opts = {} },
     "saghen/blink.cmp",
   },
   config = function()
@@ -12,7 +12,6 @@ return {
       textDocument = { completion = { completionItem = { snippetSupport = false } } },
     }
 
-    local lspconfig = require "lspconfig"
     local servers = {
       -- Vue
       volar = {
@@ -29,7 +28,7 @@ return {
             {
               name = "@vue/typescript-plugin",
               location = vim.fn.stdpath "data"
-                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                  .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
               languages = { "vue" },
             },
           },
@@ -154,8 +153,8 @@ return {
         end, opts)
         vim.keymap.set("n", "gn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-        vim.keymap.set("n", "<leader>d.", fzf.diagnostics_document, opts)
-        vim.keymap.set("n", "<leader>D.", fzf.diagnostics_workspace, opts)
+        vim.keymap.set("n", "<leader>i", fzf.diagnostics_document, opts)
+        vim.keymap.set("n", "<leader>I", fzf.diagnostics_workspace, opts)
 
         vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<cr>", opts)
         vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
@@ -198,152 +197,5 @@ return {
         end, opts)
       end,
     })
-
-    -- local auto_format = true
-
-    -- local capabilities = vim.lsp.protocol.make_client_capabilities
-    -- capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-
-    -- local servers = {
-    --   volar = {
-    --     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-    --   },
-    --   lua_ls = {
-    --     settings = {
-    --       Lua = {
-    --         runtime = {
-    --           version = "LuaJIT",
-    --         },
-    --         completion = { callSnippet = "Replace" },
-    --         diagnostics = {
-    --           globals = { "vim", "love" },
-    --         },
-    --         workspace = {
-    --           library = {
-    --             vim.env.VIMRUNTIME,
-    --           },
-    --         },
-    --       },
-    --     },
-    --   },
-    --   ruff = {},
-    --   rust_analyzer = {
-    --     checkOnSave = {
-    --       command = "clippy",
-    --     },
-    --   },
-    --   clangd = {},
-    --   -- stylua = {},
-    --   jsonls = {},
-    --   tailwindcss = {},
-    -- }
-
-    -- require("mason").setup()
-
-    -- local ensure_installed = vim.tbl_keys(servers)
-    -- require("mason-tool-installer").setup { ensure_installed = ensure_installed }
-    -- local lspconfig = require "lspconfig"
-
-    -- -- Setup mason-lspconfig
-    -- require("mason-lspconfig").setup {
-    --   ensure_installed = ensure_installed,
-    --   handlers = {
-    --     function(server_name)
-    --       local opts = servers[server_name] or {}
-    --       opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, opts.capabilities or {})
-    --       lspconfig[server_name].setup(opts)
-    --     end,
-    --   },
-    -- }
-
-    -- local auto_format = true
-    -- local is_v10 = vim.fn.has "nvim-0.10" == 1
-
-    -- vim.api.nvim_create_autocmd("LspAttach", {
-    --   group = vim.api.nvim_create_augroup("vizquit-lsp-attach", { clear = true }),
-    --   callback = function(event)
-    --     local map = vim.keymap.set
-    --     local snacks = require "snacks"
-
-    --     map("n", "gd", function()
-    --       snacks.picker.lsp_definitions()
-    --     end)
-    --     map("n", "gD", function()
-    --       snacks.picker.lsp_declarations()
-    --     end)
-    --     map("n", "gr", function()
-    --       snacks.picker.lsp_references()
-    --     end)
-    --     map("n", "gi", function()
-    --       snacks.picker.lsp_implementations()
-    --     end)
-    --     map("n", "gt", function()
-    --       snacks.picker.lsp_type_definitions()
-    --     end)
-    --     map("n", "<leader>ll", function()
-    --       snacks.picker.lsp_symbols()
-    --     end)
-    --     map("n", "<leader>lw", function()
-    --       snacks.picker.lsp_workspace_symbols()
-    --     end)
-    --     map("n", "<leader>rn", vim.lsp.buf.rename)
-    --     map("n", "<C-c>", vim.lsp.buf.code_action)
-    --     map("x", "<C-c>", vim.lsp.buf.code_action)
-    --     map("n", "gl", vim.diagnostic.open_float)
-
-    --     map("n", "<leader>li", ":LspInfo<CR>")
-    --     map("n", "<leader>lr", ":LspRestart<CR>")
-
-    --     map("n", "<leader>ff", function()
-    --       if auto_format then
-    --         auto_format = false
-    --         print "Toggling auto format OFF!"
-    --       else
-    --         print "Toggling auto format ON!"
-    --         auto_format = true
-    --       end
-    --       -- auto_format = not auto_format -- toggle it
-    --     end)
-
-    --     map("n", "<leader>k", function()
-    --       if is_v10 then
-    --         ---@diagnostic disable-next-line: deprecated
-    --         vim.diagnostic.goto_prev()
-    --       else
-    --         vim.diagnostic.jump { count = -1 }
-    --       end
-    --       vim.cmd "norm zz"
-    --     end)
-    --     map("n", "<leader>j", function()
-    --       if is_v10 then
-    --         ---@diagnostic disable-next-line: deprecated
-    --         vim.diagnostic.goto_next()
-    --       else
-    --         vim.diagnostic.jump { count = 1 }
-    --       end
-    --       vim.cmd "norm zz"
-    --     end)
-
-    --     map("n", "<leader>v", function()
-    --       vim.cmd "vsplit | lua vim.lsp.buf.definition()"
-    --       vim.cmd "norm zz"
-    --     end)
-
-    --     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    --     if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-    --       local highlight_group = vim.api.nvim_create_augroup("vizquit-lsp-highlight", { clear = false })
-    --       vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-    --         buffer = event.buf,
-    --         group = highlight_group,
-    --         callback = vim.lsp.buf.document_highlight,
-    --       })
-    --       vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-    --         buffer = event.buf,
-    --         group = highlight_group,
-    --         callback = vim.lsp.buf.clear_references,
-    --       })
-    --     end
-    --   end,
-    -- })
   end,
 }
