d90e964a983648eb43dfdebe2a86a13aa0b66a53
emanuel2718 <eramirez2718@gmail.com>
2025-04-09 21:47:47 -0400

    get over it

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -5,27 +5,19 @@ return {
   dependencies = { "echasnovski/mini.icons" },
   config = function()
     local fzf = require "fzf-lua"
-    fzf.setup({ { "fzf-vim", "ivy" } }, {
-      -- files = {
-      --   fd_opts = "--color=never --type f --hidden --follow --exclude .git",
-      --   rg_opts = "--color=never --files --hidden --follow -g '!.git'",
-      --   find_opts = "-type f -not -path '*/.git/*'",
-      -- },
-      -- grep = {
-      --   rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=512",
-      --   no_esc_esc = true,
-      -- },
+    fzf.setup({ { "max-perf", "ivy" } }, {
+      files = {
+        previewer = false
+      }
     })
     local map = vim.keymap.set
-    map("n", "<leader>.", fzf.files)
-    map("n", "<leader>ff", function()
-      fzf.files { hidden = true }
-    end)
+    map("n", "<leader>.", function() fzf.files({ previewer = false }) end)
     map("n", "<leader>fo", fzf.oldfiles)
     map("n", "<leader><leader>", fzf.buffers)
     map("n", "<leader>hh", fzf.helptags)
     map("n", "<leader>/", fzf.grep_curbuf)
-    map("n", "<leader>sp", fzf.live_grep_native)
+    map("n", "<leader>sp", function() fzf.live_grep({ hidden = true }) end)
+    map("n", "<leader>s/", fzf.live_grep_native)
     map("n", "<leader>s.", fzf.grep_cWORD)
     map("n", "<leader>,", fzf.resume)
     map("n", "<leader>ht", fzf.colorschemes)
