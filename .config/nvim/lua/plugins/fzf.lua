fecb9604924cf5bc31849431f47afe23dc04ac31
emanuel2718 <eramirez2718@gmail.com>
2025-04-30 08:02:24 -0400

    add trouble to nvim

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -22,7 +22,8 @@ return {
       fzf.live_grep { hidden = true }
     end)
     map("n", "<leader>s/", fzf.live_grep_native)
-    map("n", "<leader>s.", fzf.grep_cWORD)
+    map("n", "<leader>s.", fzf.grep_cword)
+    map("v", "<leader>s.", fzf.grep_visual)
     map("n", "<leader>,", fzf.resume)
     map("n", "<leader>ht", fzf.colorschemes)
     map("n", "<leader>sf", function()
